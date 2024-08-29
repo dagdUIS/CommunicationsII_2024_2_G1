72095792f10e408d45ca66b1463cbe16c41a73de
Grugo_Banco_01 <diego2195533@correo.uis.edu.co>
2024-08-29 11:54:11 -0500

    Clase 29 de agosto

--- Practica_1/Practica1/GNURadio/diagramaBloques.py
+++ Practica_1/Practica1/GNURadio/diagramaBloques.py
@@ -6,40 +6,26 @@
 #
 # GNU Radio Python Flow Graph
 # Title: Practica_1
-# GNU Radio version: 3.9.8.0
-
-from distutils.version import StrictVersion
-
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print("Warning: failed to XInitThreads()")
+# GNU Radio version: 3.10.9.2
 
 from PyQt5 import Qt
 from gnuradio import qtgui
-from gnuradio.filter import firdes
-import sip
 from gnuradio import analog
 from gnuradio import blocks
-import numpy
 from gnuradio import gr
+from gnuradio.filter import firdes
 from gnuradio.fft import window
 import sys
 import signal
+from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-import E3TRadio
-import diagramaBloques_epy_block_0 as epy_block_0  # embedded python block
+import diagramaBloques_epy_block_0_0 as epy_block_0_0  # embedded python block
+import diagramaBloques_epy_block_2_0 as epy_block_2_0  # embedded python block
+import sip
 
 
-
-from gnuradio import qtgui
 
 class diagramaBloques(gr.top_block, Qt.QWidget):
 
@@ -50,8 +36,8 @@ class diagramaBloques(gr.top_block, Qt.QWidget):
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
         self.top_scroll_layout = Qt.QVBoxLayout()
         self.setLayout(self.top_scroll_layout)
         self.top_scroll = Qt.QScrollArea()
@@ -67,12 +53,11 @@ class diagramaBloques(gr.top_block, Qt.QWidget):
         self.settings = Qt.QSettings("GNU Radio", "diagramaBloques")
 
         try:
-            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-                self.restoreGeometry(self.settings.value("geometry").toByteArray())
-            else:
-                self.restoreGeometry(self.settings.value("geometry"))
-        except:
-            pass
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
 
         ##################################################
         # Variables
@@ -82,30 +67,79 @@ class diagramaBloques(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
-            1024, #size
+
+        self.qtgui_time_sink_x_1_0 = qtgui.time_sink_f(
+            2048, #size
             samp_rate, #samp_rate
             "", #name
             1, #number of inputs
             None # parent
         )
-        self.qtgui_time_sink_x_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+        self.qtgui_time_sink_x_1_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_1_0.set_y_axis(-1, 1)
 
-        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
+        self.qtgui_time_sink_x_1_0.set_y_label('Amplitude', "")
 
-        self.qtgui_time_sink_x_0.enable_tags(True)
-        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(False)
-        self.qtgui_time_sink_x_0.enable_grid(False)
-        self.qtgui_time_sink_x_0.enable_axis_labels(True)
-        self.qtgui_time_sink_x_0.enable_control_panel(False)
-        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+        self.qtgui_time_sink_x_1_0.enable_tags(True)
+        self.qtgui_time_sink_x_1_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_1_0.enable_autoscale(True)
+        self.qtgui_time_sink_x_1_0.enable_grid(True)
+        self.qtgui_time_sink_x_1_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_1_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_1_0.enable_stem_plot(False)
 
 
-        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
+        labels = ['Diferenciador', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
             'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
-        widths = [1, 1, 1, 1, 1,
+        widths = [2, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['red', 'red', 'green', 'black', 'cyan',
+            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_1_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_1_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_1_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_1_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_1_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_1_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_1_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_1_0_win = sip.wrapinstance(self.qtgui_time_sink_x_1_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_1_0_win)
+        self.qtgui_time_sink_x_1 = qtgui.time_sink_f(
+            2048, #size
+            samp_rate, #samp_rate
+            "", #name
+            1, #number of inputs
+            None # parent
+        )
+        self.qtgui_time_sink_x_1.set_update_time(0.10)
+        self.qtgui_time_sink_x_1.set_y_axis(-1, 1)
+
+        self.qtgui_time_sink_x_1.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_1.enable_tags(True)
+        self.qtgui_time_sink_x_1.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_1.enable_autoscale(True)
+        self.qtgui_time_sink_x_1.enable_grid(True)
+        self.qtgui_time_sink_x_1.enable_axis_labels(True)
+        self.qtgui_time_sink_x_1.enable_control_panel(False)
+        self.qtgui_time_sink_x_1.enable_stem_plot(False)
+
+
+        labels = ['Acumulador', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
+            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
+        widths = [2, 1, 1, 1, 1,
             1, 1, 1, 1, 1]
         colors = ['blue', 'red', 'green', 'black', 'cyan',
             'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
@@ -119,66 +153,99 @@ class diagramaBloques(gr.top_block, Qt.QWidget):
 
         for i in range(1):
             if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+                self.qtgui_time_sink_x_1.set_line_label(i, "Data {0}".format(i))
             else:
-                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
-            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
-            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+                self.qtgui_time_sink_x_1.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_1.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_1.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_1.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_1.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_1.set_line_alpha(i, alphas[i])
 
-        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.epy_block_0 = epy_block_0.blk()
-        self.blocks_int_to_float_0 = blocks.int_to_float(1, 1)
-        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, 1000, 1, 0, 0)
-        self.analog_random_source_x_0 = blocks.vector_source_i(list(map(int, numpy.random.randint(0, 2, 1000))), True)
-        self.E3TRadio_sumador_0 = E3TRadio.sumador()
-        self.Acumulador = qtgui.number_sink(
+        self._qtgui_time_sink_x_1_win = sip.wrapinstance(self.qtgui_time_sink_x_1.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_1_win)
+        self.epy_block_2_0 = epy_block_2_0.blk()
+        self.epy_block_0_0 = epy_block_0_0.blk()
+        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_float*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.analog_sig_source_x_1 = analog.sig_source_f(samp_rate, analog.GR_TRI_WAVE, 100, 1, (-0.5), 0)
+        self.Salida_Dif = qtgui.number_sink(
             gr.sizeof_float,
             0,
             qtgui.NUM_GRAPH_HORIZ,
             1,
             None # parent
         )
-        self.Acumulador.set_update_time(0.10)
-        self.Acumulador.set_title("")
+        self.Salida_Dif.set_update_time(0.10)
+        self.Salida_Dif.set_title("")
 
-        labels = ['', '', '', '', '',
+        labels = ['Diferenciador', '', '', '', '',
             '', '', '', '', '']
         units = ['', '', '', '', '',
             '', '', '', '', '']
-        colors = [("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"),
+        colors = [("black", "red"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"),
             ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black")]
-        factor = [1, 1, 1, 1, 1,
+        factor = [2, 1, 1, 1, 1,
             1, 1, 1, 1, 1]
 
         for i in range(1):
-            self.Acumulador.set_min(i, -1)
-            self.Acumulador.set_max(i, 1)
-            self.Acumulador.set_color(i, colors[i][0], colors[i][1])
+            self.Salida_Dif.set_min(i, -1)
+            self.Salida_Dif.set_max(i, 1)
+            self.Salida_Dif.set_color(i, colors[i][0], colors[i][1])
             if len(labels[i]) == 0:
-                self.Acumulador.set_label(i, "Data {0}".format(i))
+                self.Salida_Dif.set_label(i, "Data {0}".format(i))
             else:
-                self.Acumulador.set_label(i, labels[i])
-            self.Acumulador.set_unit(i, units[i])
-            self.Acumulador.set_factor(i, factor[i])
+                self.Salida_Dif.set_label(i, labels[i])
+            self.Salida_Dif.set_unit(i, units[i])
+            self.Salida_Dif.set_factor(i, factor[i])
 
-        self.Acumulador.enable_autoscale(False)
-        self._Acumulador_win = sip.wrapinstance(self.Acumulador.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._Acumulador_win)
+        self.Salida_Dif.enable_autoscale(False)
+        self._Salida_Dif_win = sip.wrapinstance(self.Salida_Dif.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._Salida_Dif_win)
+        self.Salida_Acum = qtgui.number_sink(
+            gr.sizeof_float,
+            0,
+            qtgui.NUM_GRAPH_HORIZ,
+            1,
+            None # parent
+        )
+        self.Salida_Acum.set_update_time(0.10)
+        self.Salida_Acum.set_title("")
+
+        labels = ['Acumulador', '', '', '', '',
+            '', '', '', '', '']
+        units = ['', '', '', '', '',
+            '', '', '', '', '']
+        colors = [("blue", "red"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"),
+            ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black")]
+        factor = [2, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+
+        for i in range(1):
+            self.Salida_Acum.set_min(i, -1)
+            self.Salida_Acum.set_max(i, 1)
+            self.Salida_Acum.set_color(i, colors[i][0], colors[i][1])
+            if len(labels[i]) == 0:
+                self.Salida_Acum.set_label(i, "Data {0}".format(i))
+            else:
+                self.Salida_Acum.set_label(i, labels[i])
+            self.Salida_Acum.set_unit(i, units[i])
+            self.Salida_Acum.set_factor(i, factor[i])
+
+        self.Salida_Acum.enable_autoscale(False)
+        self._Salida_Acum_win = sip.wrapinstance(self.Salida_Acum.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._Salida_Acum_win)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.E3TRadio_sumador_0, 0), (self.epy_block_0, 0))
-        self.connect((self.analog_random_source_x_0, 0), (self.blocks_int_to_float_0, 0))
-        self.connect((self.analog_sig_source_x_0, 0), (self.E3TRadio_sumador_0, 0))
-        self.connect((self.blocks_int_to_float_0, 0), (self.E3TRadio_sumador_0, 1))
-        self.connect((self.epy_block_0, 0), (self.Acumulador, 0))
-        self.connect((self.epy_block_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.analog_sig_source_x_1, 0), (self.blocks_throttle2_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.epy_block_0_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.epy_block_2_0, 0))
+        self.connect((self.epy_block_0_0, 0), (self.Salida_Acum, 0))
+        self.connect((self.epy_block_0_0, 0), (self.qtgui_time_sink_x_1, 0))
+        self.connect((self.epy_block_2_0, 0), (self.Salida_Dif, 0))
+        self.connect((self.epy_block_2_0, 0), (self.qtgui_time_sink_x_1_0, 0))
 
 
     def closeEvent(self, event):
@@ -194,17 +261,16 @@ class diagramaBloques(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+        self.analog_sig_source_x_1.set_sampling_freq(self.samp_rate)
+        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
+        self.qtgui_time_sink_x_1.set_samp_rate(self.samp_rate)
+        self.qtgui_time_sink_x_1_0.set_samp_rate(self.samp_rate)
 
 
 
 
 def main(top_block_cls=diagramaBloques, options=None):
 
-    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
     qapp = Qt.QApplication(sys.argv)
 
     tb = top_block_cls()
