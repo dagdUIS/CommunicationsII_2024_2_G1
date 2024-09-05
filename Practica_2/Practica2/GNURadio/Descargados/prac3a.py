34acd2276315f5f5bfddd5105a6230c77eb1b28d
Comu2_GrupoC1 <diego2195533@correo.uis.edu.co>
2024-09-05 11:57:10 -0500

    Evidencias trabajo en clase LAB

--- Practica_2/Practica2/GNURadio/Descargados/prac3a.py
+++ Practica_2/Practica2/GNURadio/Descargados/prac3a.py
@@ -7,42 +7,30 @@
 # GNU Radio Python Flow Graph
 # Title: Not titled yet
 # Author: radiogis_director
-# GNU Radio version: 3.9.0.0
-
-from distutils.version import StrictVersion
-
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print("Warning: failed to XInitThreads()")
+# GNU Radio version: v3.11.0.0git-810-g1ecb8565
 
 from PyQt5 import Qt
 from gnuradio import qtgui
-from gnuradio.filter import firdes
-import sip
 from gnuradio import analog
 from gnuradio import blocks
 import numpy
 from gnuradio import fft
 from gnuradio.fft import window
 from gnuradio import filter
+from gnuradio.filter import firdes
 from gnuradio import gr
 import sys
 import signal
+from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-import epy_block_0
 import numpy as np
+import prac3a_epy_block_0 as epy_block_0  # embedded python block
+import sip
+import threading
 
 
-
-from gnuradio import qtgui
 
 class prac3a(gr.top_block, Qt.QWidget):
 
@@ -53,8 +41,8 @@ class prac3a(gr.top_block, Qt.QWidget):
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
         self.top_scroll_layout = Qt.QVBoxLayout()
         self.setLayout(self.top_scroll_layout)
         self.top_scroll = Qt.QScrollArea()
@@ -67,15 +55,15 @@ class prac3a(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "prac3a")
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "prac3a")
 
         try:
-            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-                self.restoreGeometry(self.settings.value("geometry").toByteArray())
-            else:
-                self.restoreGeometry(self.settings.value("geometry"))
-        except:
-            pass
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
+        self.flowgraph_started = threading.Event()
 
         ##################################################
         # Variables
@@ -89,6 +77,7 @@ class prac3a(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
+
         self.Menu = Qt.QTabWidget()
         self.Menu_widget_0 = Qt.QWidget()
         self.Menu_layout_0 = Qt.QBoxLayout(Qt.QBoxLayout.TopToBottom, self.Menu_widget_0)
@@ -100,11 +89,11 @@ class prac3a(gr.top_block, Qt.QWidget):
         self.Menu_grid_layout_1 = Qt.QGridLayout()
         self.Menu_layout_1.addLayout(self.Menu_grid_layout_1)
         self.Menu.addTab(self.Menu_widget_1, 'Freq')
-        self.top_grid_layout.addWidget(self.Menu)
+        self.top_layout.addWidget(self.Menu)
         self.qtgui_vector_sink_f_0 = qtgui.vector_sink_f(
             N,
-            -samp_rate/2,
-            samp_rate/N,
+            (-samp_rate/2),
+            (samp_rate/N),
             "f",
             "Sx(f)",
             "PSD (Watts/Hz)",
@@ -112,12 +101,13 @@ class prac3a(gr.top_block, Qt.QWidget):
             None # parent
         )
         self.qtgui_vector_sink_f_0.set_update_time(0.10)
-        self.qtgui_vector_sink_f_0.set_y_axis(0, 1/Rb)
+        self.qtgui_vector_sink_f_0.set_y_axis(0, (1/Rb))
         self.qtgui_vector_sink_f_0.enable_autoscale(False)
         self.qtgui_vector_sink_f_0.enable_grid(False)
         self.qtgui_vector_sink_f_0.set_x_axis_units("Hz")
         self.qtgui_vector_sink_f_0.set_y_axis_units("Watss/Hz")
         self.qtgui_vector_sink_f_0.set_ref_level(0)
+
 
         labels = ['p4', '', '', '', '',
             '', '', '', '', '']
@@ -137,14 +127,14 @@ class prac3a(gr.top_block, Qt.QWidget):
             self.qtgui_vector_sink_f_0.set_line_color(i, colors[i])
             self.qtgui_vector_sink_f_0.set_line_alpha(i, alphas[i])
 
-        self._qtgui_vector_sink_f_0_win = sip.wrapinstance(self.qtgui_vector_sink_f_0.pyqwidget(), Qt.QWidget)
+        self._qtgui_vector_sink_f_0_win = sip.wrapinstance(self.qtgui_vector_sink_f_0.qwidget(), Qt.QWidget)
         self.Menu_grid_layout_0.addWidget(self._qtgui_vector_sink_f_0_win, 3, 0, 1, 1)
         for r in range(3, 4):
             self.Menu_grid_layout_0.setRowStretch(r, 1)
         for c in range(0, 1):
             self.Menu_grid_layout_0.setColumnStretch(c, 1)
         self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
-            16*Sps, #size
+            (16*Sps), #size
             samp_rate, #samp_rate
             "", #name
             1, #number of inputs
@@ -189,7 +179,7 @@ class prac3a(gr.top_block, Qt.QWidget):
             self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
             self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
 
-        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
         self.Menu_grid_layout_0.addWidget(self._qtgui_time_sink_x_0_win, 2, 0, 1, 1)
         for r in range(2, 3):
             self.Menu_grid_layout_0.setRowStretch(r, 1)
@@ -205,10 +195,10 @@ class prac3a(gr.top_block, Qt.QWidget):
             None # parent
         )
         self.qtgui_freq_sink_x_0.set_update_time(0.10)
-        self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
+        self.qtgui_freq_sink_x_0.set_y_axis((-140), 10)
         self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
         self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
-        self.qtgui_freq_sink_x_0.enable_autoscale(False)
+        self.qtgui_freq_sink_x_0.enable_autoscale(True)
         self.qtgui_freq_sink_x_0.enable_grid(False)
         self.qtgui_freq_sink_x_0.set_fft_average(1.0)
         self.qtgui_freq_sink_x_0.enable_axis_labels(True)
@@ -236,7 +226,7 @@ class prac3a(gr.top_block, Qt.QWidget):
             self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
             self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
 
-        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
+        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
         self.Menu_grid_layout_1.addWidget(self._qtgui_freq_sink_x_0_win, 3, 0, 1, 1)
         for r in range(3, 4):
             self.Menu_grid_layout_1.setRowStretch(r, 1)
@@ -255,16 +245,17 @@ class prac3a(gr.top_block, Qt.QWidget):
         self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
         self.blocks_add_xx_0 = blocks.add_vff(1)
         self.analog_random_source_x_0 = blocks.vector_source_b(list(map(int, numpy.random.randint(0, 2, 1000000))), True)
-        self.analog_noise_source_x_0 = analog.noise_source_f(analog.GR_GAUSSIAN, 1, 76)
-        self.analog_const_source_x_0_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, -1./2.)
-
+        self.analog_noise_source_x_0 = analog.noise_source_f(analog.GR_GAUSSIAN, 5, 76)
+        self.analog_const_source_x_0_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, (-1./2.))
 
 
         ##################################################
         # Connections
         ##################################################
         self.connect((self.analog_const_source_x_0_0, 0), (self.blocks_add_xx_0, 0))
-        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_null_sink_0, 0))
+        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_stream_to_vector_0, 0))
+        self.connect((self.analog_noise_source_x_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.analog_noise_source_x_0, 0), (self.qtgui_time_sink_x_0, 0))
         self.connect((self.analog_random_source_x_0, 0), (self.blocks_char_to_float_0, 0))
         self.connect((self.blocks_add_xx_0, 0), (self.blocks_multiply_const_vxx_0, 0))
         self.connect((self.blocks_char_to_float_0, 0), (self.blocks_add_xx_0, 1))
@@ -272,16 +263,14 @@ class prac3a(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_multiply_const_vxx_0, 0), (self.interp_fir_filter_xxx_0, 0))
         self.connect((self.blocks_multiply_const_vxx_1, 0), (self.qtgui_vector_sink_f_0, 0))
         self.connect((self.blocks_stream_to_vector_0, 0), (self.fft_vxx_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.blocks_stream_to_vector_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_null_sink_0, 0))
         self.connect((self.epy_block_0, 0), (self.blocks_multiply_const_vxx_1, 0))
         self.connect((self.fft_vxx_0, 0), (self.blocks_complex_to_mag_squared_0, 0))
         self.connect((self.interp_fir_filter_xxx_0, 0), (self.blocks_throttle_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "prac3a")
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "prac3a")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
@@ -309,7 +298,7 @@ class prac3a(gr.top_block, Qt.QWidget):
     def set_Rb(self, Rb):
         self.Rb = Rb
         self.set_samp_rate(self.Rb*self.Sps)
-        self.qtgui_vector_sink_f_0.set_y_axis(0, 1/self.Rb)
+        self.qtgui_vector_sink_f_0.set_y_axis(0, (1/self.Rb))
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -320,7 +309,7 @@ class prac3a(gr.top_block, Qt.QWidget):
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
         self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
-        self.qtgui_vector_sink_f_0.set_x_axis(-self.samp_rate/2, self.samp_rate/self.N)
+        self.qtgui_vector_sink_f_0.set_x_axis((-self.samp_rate/2), (self.samp_rate/self.N))
 
     def get_N(self):
         return self.N
@@ -329,21 +318,20 @@ class prac3a(gr.top_block, Qt.QWidget):
         self.N = N
         self.blocks_multiply_const_vxx_1.set_k([1/(self.N*self.samp_rate)]*self.N)
         self.epy_block_0.N = self.N
-        self.qtgui_vector_sink_f_0.set_x_axis(-self.samp_rate/2, self.samp_rate/self.N)
+        self.fft_vxx_0.set_window([1]*self.N)
+        self.qtgui_vector_sink_f_0.set_x_axis((-self.samp_rate/2), (self.samp_rate/self.N))
 
 
 
 
 def main(top_block_cls=prac3a, options=None):
 
-    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
     qapp = Qt.QApplication(sys.argv)
 
     tb = top_block_cls()
 
     tb.start()
+    tb.flowgraph_started.set()
 
     tb.show()
 
